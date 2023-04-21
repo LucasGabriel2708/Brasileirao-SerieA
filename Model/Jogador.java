d1f5efbf2c7e123484405f41b6018df2ade8c691
lucasdias <lucas.dias@arapiraca.ufal.br>
2023-04-21 14:13:10 -0300

    Adições no código

--- Model/Jogador.java
+++ Model/Jogador.java
@@ -1,5 +1,34 @@
 package Model;
 
 public class Jogador {
+   private int id;
+   private String nome;
+   private String posicao;
+   private int numero;
+   private Time time;
+
+// CONSTRUTOR DE JOGADOR
+   public Jogador(int id, String nome, String posicao, int numero, Time time) {
+      this.id = id;
+      this.nome = nome;
+      this.posicao = posicao;
+      this.numero = numero;
+      this.time = time;
+   }
+
+// SETA UM NUMERO NUM JOGADOR
+   public void setPosicao(String posicao){
+      this.posicao = posicao;
+   }
+
+// SETA UM NUMERO NUM JOGADOR
+   public void setNumero(int numero){
+      this.numero = numero;
+   }
+
+// RECUPERA O NOME DO JOGADOR, USADO EM TIME PARA PERCORRER A LISTA
+   public int getIdJogador() {
+      return id;
+   }
     
 }
--- Model/Partida.java
+++ Model/Partida.java
@@ -1,5 +1,32 @@
 package Model;
 
+import java.sql.Date;
+
 public class Partida {
+   private int id;
+   private Date data;
+   private Time mandante;
+   private Time visitante;
+   private int golsMandante;
+   private int golsVisitante;
+
+
+   public Partida(int id, Date data, Time mandante, Time visitante, int golsMandante, int golsVisitante) {
+      this.id = id;
+      this.data = data;
+      this.mandante = mandante;
+      this.visitante = visitante;
+      this.golsMandante = golsMandante;
+      this.golsVisitante = golsVisitante;
+   }
+
+   public void setGolsVisitante(int gols){
+      this.golsVisitante = golsVisitante;
+   }
+
+   public void setGolsMandante(int gols){
+      this.golsMandante = golsMandante;
+   }
+
     
 }
